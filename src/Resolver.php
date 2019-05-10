48daa1a7b4ee4ad746b2b02e6a032a73359a3aad
Payton Bice <hellopayton@gmail.com>
2019-05-10 15:56:59 -0500

    DependencyResolverInterface -> Resolver

--- src/Resolver.php
+++ src/Resolver.php
@@ -5,7 +5,7 @@ namespace p810\Container;
 /**
  * Classes that implement this interface should be able to instantiate classes and their dependencies to automate dependency injection.
  */
-interface DependencyResolverInterface
+interface Resolver
 {
     /**
      * Instantiates a class and its dependencies
@@ -14,13 +14,4 @@ interface DependencyResolverInterface
      * @return object
      */
     public function resolve(string $className): object;
-
-    /**
-     * Binds an interface to a specific implementation
-     * 
-     * @param string $interfaceName
-     * @param string $className
-     * @return void
-     */
-    public function bind(string $interfaceName, string $className): void;
 }
