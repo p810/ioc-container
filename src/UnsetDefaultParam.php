b55e6a999b8e6ef474a3e0f725c3bd67000f218b
Payton Bice <hellopayton@gmail.com>
2019-05-13 15:39:59 -0500

    Adds documentation and fixes UnsetDefaultParam I'd forgotten to actually make the singleton, a singleton...

--- src/UnsetDefaultParam.php
+++ src/UnsetDefaultParam.php
@@ -2,22 +2,38 @@
 
 namespace p810\Container;
 
+/**
+ * Represents a case where a user has not specified a default value for a parameter in
+ * an object's constructor
+ */
 class UnsetDefaultParam
 {
+    /**
+     * @var \p810\Container\UnsetDefaultParam
+     */
     static $instance;
 
-    private function __construct()
-    {
-    }
-
-    private function __clone()
-    {
-    }
-
+    /**
+     * Returns a single instance of this class
+     * 
+     * @return \p810\Container\UnsetDefaultParam
+     */
     public static function getInstance()
     {
-        self::$instance = new UnsetDefaultParam;
+        if (! self::$instance instanceof UnsetDefaultParam) {
+            self::$instance = new UnsetDefaultParam;
+        }
 
         return self::$instance;
     }
+
+    /**
+     * Prevent direct instantiation and cloning of this object
+     * 
+     * @return void
+     */
+    private function __clone()
+    {}
+    private function __construct()
+    {}
 }
