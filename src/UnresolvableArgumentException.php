4ca77bca6d139168ec8866a477f7a783f635867d
Payton Bice <hellopayton@gmail.com>
2019-05-13 19:39:18 -0500

    Adds a description to UnresolvableArgumentException

--- src/UnresolvableArgumentException.php
+++ src/UnresolvableArgumentException.php
@@ -4,6 +4,9 @@ namespace p810\Container;
 
 use RuntimeException;
 
+/**
+ * Exception thrown if a parameter of a class's constructor couldn't be resolved when attempting to instantiate it.
+ */
 class UnresolvableArgumentException extends RuntimeException
 {
 }
